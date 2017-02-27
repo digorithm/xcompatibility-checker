32d919a77f9ffc72d10c6373b2ca1e3f3e285c01
marquesarthur <marques.art@gmail.com>
2017-02-26 18:23:58 -0800

    Git analysis. Got dataset with active projects

---
+++ analyzer.js
@@ -0,0 +1,8 @@
+/*
+ Analyzer is a tool to check both the overall cross-browser compatibility of github projects
+ as well as their historical cross-browser compatibility net-worth, i.e. if their compatibility
+ increases or decreases overtime.
+
+ Usage:
+ - Go to your terminal, move to the root folder, run `node git_analysis/analyzer.js`
+ */
