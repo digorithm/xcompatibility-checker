708fa93fb396ac310d70f72cca569d0e2afae956
marquesarthur <marques.art@gmail.com>
2017-03-07 12:06:24 -0800

    Final update on git_projects.json to run analyzer script

--- git_analysis/analyzer.js
+++ git_analysis/analyzer.js
@@ -6,3 +6,54 @@
  Usage:
  - Go to your terminal, move to the root folder, run `node git_analysis/analyzer.js`
  */
+
+const fs = require('fs');
+const Promise = require('promise');
+const Git = require("nodegit");
+
+const config = require('../git.config.json');
+
+const projects_file = 'git_projects.json';
+const tmp_dir = 'git_analysis/tmp';
+const projects = JSON.parse(fs.readFileSync(projects_file));
+
+
+function makeDir(dir_path) {
+    if (!fs.existsSync(dir_path)){
+        fs.mkdirSync(dir_path);
+    }
+}
+
+function getGitRepositoryName(url) {
+    var idx = url.lastIndexOf('/');
+    if (idx == -1){
+        return undefined;
+    }
+    return url.substring(idx + 1, url.length);
+
+}
+
+function cloneRepos() {
+    var promises = [];
+    projects.forEach(function (git_project) {
+
+        if (typeof git_project.active !== 'undefined' && git_project.active) {
+
+            var repo = getGitRepositoryName(git_project.git);
+            var path = tmp_dir + '/' + repo;
+
+            var p = Git.Clone(git_project.git, path);
+
+            promises.push(p);
+        }
+    });
+
+
+
+    Promise.all(promises).then(function() {
+        console.log('Done');
+    });
+}
+
+makeDir(tmp_dir);
+cloneRepos();
